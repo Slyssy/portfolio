f0db914568695a890ec0a55941e86e563200b085
Stephen <stephen.lyssy@gmail.com>
2022-06-21 06:03:41 -0500

    Updated tablet css styling.

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
 const themeSwitchers = document.querySelectorAll('span');
 const dynamicInputs = document.querySelectorAll('input.input-color-picker');
-
-let cssPropName;
+const landingH1s = document.querySelectorAll('#hero h1 span');
+console.log(landingH1s);
 const handleThemeUpdate = (cssVars) => {
   const root = document.querySelector(':root');
   const keys = Object.keys(cssVars);
   keys.forEach((key) => {
+    inputColor = cssVars[key];
     root.style.setProperty(key, cssVars[key]);
+    console.log(cssVars[key]);
   });
 };
-
-// themeSwitchers.forEach((item) => {
-//   item.addEventListener('click', (e) => {
-//     const themeColor = e.target.getAttribute('data-bg-color');
-
-//     handleThemeUpdate({
-//       '--primary-color': themeColor,
-//     });
-
-//     $("input.input-color-picker[data-id='theme-color']").val(themeColor);
-//   });
-// });
 
 dynamicInputs.forEach((item) => {
   item.addEventListener('input', (e) => {
     const cssPropName = `--primary-${e.target.getAttribute('data-id')}`;
+    console.log(cssPropName);
+    console.log(e.target.getAttribute('data-id'));
     handleThemeUpdate({
       [cssPropName]: e.target.value,
     });
   });
 });
+
+// const triggerAnimations = function () {
+//   landingH1s.forEach((h1) => h1.classList.toggle('animate'));
+// };
